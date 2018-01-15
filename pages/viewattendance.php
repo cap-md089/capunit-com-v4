0edcc942db5e88df3429859eff2706f3b7cd0d15
Glenn Rioux (LapHP) <grioux@gmail.com>
2018-01-15 04:16:07 -0500

    Added end date/time to view attendance data

--- pages/viewattendance.php
+++ pages/viewattendance.php
@@ -10,7 +10,7 @@
 
             $tblAtt = DB_TABLES['Attendance'];
             $tblEvt = DB_TABLES['EventInformation'];
-            $sql = "SELECT $tblAtt.*, $tblEvt.EventLocation, $tblEvt.StartDateTime FROM $tblEvt INNER JOIN ";
+            $sql = "SELECT $tblAtt.*, $tblEvt.EventLocation, $tblEvt.StartDateTime, $tblEvt.EndDateTime FROM $tblEvt INNER JOIN ";
             $sql .= "$tblAtt ON $tblAtt.EventID = $tblEvt.EventNumber WHERE $tblAtt.AccountID=:aid AND ";
             $sql .= "$tblEvt.AccountID=:aid AND $tblAtt.CAPID=:cid AND $tblAtt.Status='Committed/Attended' ";
             $sql .= "ORDER BY StartDateTime DESC;";
@@ -27,9 +27,10 @@
             $columns[0] = "Event Number";
             $columns[1] = "Event Location";
             $columns[2] = "Start Date/Time";
-            $columns[3] = "Plan to use CAP Transport";
-            $columns[4] = "Comments";
-            for ($line = "", $i = 0 ; $i < 5 ; $i++) { $line .= $columns[$i]."\t"; }
+            $columns[3] = "End Date/Time";
+            $columns[4] = "Plan to use CAP Transport";
+            $columns[5] = "Comments";
+            for ($line = "", $i = 0 ; $i < 6 ; $i++) { $line .= $columns[$i]."\t"; }
             $line = substr($line, 0, strlen($line) - 2)."";
             // for ($line = "", $i = 0 ; $i < 5 ; $i++) { $line .= $columns[$i].","; }
             // $line = substr($line, 0, strlen($line) - 2);
@@ -39,10 +40,11 @@
                 $columns[0] = $a->id."-".$datum['EventID'];
                 $columns[1] = $datum['EventLocation'];
                 $columns[2] = date('d M Y, H:i',$datum['StartDateTime']);
-                if(!$datum['PlanToUseCAPTransportation']) {$columns[3]='false';} else {$columns[3]='true';}
-                $columns[4] = $datum['Comments'];
+                $columns[3] = "End Date/Time";
+                if(!$datum['PlanToUseCAPTransportation']) {$columns[4]='false';} else {$columns[4]='true';}
+                $columns[5] = $datum['Comments'];
 
-                for ($line = "", $i = 0 ; $i < 5 ; $i++) { $line .= $columns[$i]."\t"; }
+                for ($line = "", $i = 0 ; $i < 6 ; $i++) { $line .= $columns[$i]."\t"; }
                 $line = substr($line, 0, strlen($line) - 2)."";
                 $attendanceData .= $line."\r\n";
                 // for ($line = "", i = 0 ; i <= 5 ; i++) { $line .= $columns[i].","; }
